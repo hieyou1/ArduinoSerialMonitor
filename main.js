3fa95e18dd6a70b8feabb5c9266e1eac201fbe22
Dalmir <dalmirdasilva@gmail.com>
2022-03-10 17:12:18 -0700

    Solve new line bug.

--- main.js
+++ main.js
@@ -12,105 +12,111 @@ var previousPaths;
 var lineEndingSelect;
 
 function initComponents() {
-  autoScrollInput = document.getElementById("auto-scroll");
-  statusText = document.getElementById("status");
-  sendButton = document.getElementById("send");
-  outputTextArea = document.getElementById("output");
-  inputText = document.getElementById("input");
-  portsSelect = document.getElementById("ports");
-  baudrateSelect = document.getElementById("baudrate");
-  connectButton = document.getElementById("connect");
-  lineEndingSelect = document.getElementById("line-ending");
-  monitor = new Monitor();
+    autoScrollInput = document.getElementById("auto-scroll");
+    statusText = document.getElementById("status");
+    sendButton = document.getElementById("send");
+    outputTextArea = document.getElementById("output");
+    inputText = document.getElementById("input");
+    portsSelect = document.getElementById("ports");
+    baudrateSelect = document.getElementById("baudrate");
+    connectButton = document.getElementById("connect");
+    lineEndingSelect = document.getElementById("line-ending");
+    monitor = new Monitor();
 }
 
 function listPorts(ports) {
-  portsSelect.innerHTML = "";
-  var options = portsSelect.options;
-  ports.map(function (p) {
-    options[options.length] = new Option(p.path, p.path, false, (port && port.isConnected() && port.getPath() == p));
-  });
+    portsSelect.innerHTML = "";
+    var options = portsSelect.options;
+    ports.map(function (p) {
+        options[options.length] = new Option(p.path, p.path, false, (port && port.isConnected() && port.getPath() === p));
+    });
 }
 
 function connect(path, baudrate) {
-  if (port && port.isConnected()) {
-    port.disconnect();
-  } else {
-    outputTextArea.innerHTML = ""; 
-    port = new SerialPort(path, {
-      bitrate: parseInt(baudrate),
-      ctsFlowControl: true
-    });
-    port.addEventListener(SerialPort.EVENT.STATE_CHANGE, {notify: notify});
-    port.addEventListener(SerialPort.EVENT.DATA_AVAILABLE, monitor);
-    port.connect();
-  }
+    if (port && port.isConnected()) {
+        port.disconnect();
+    } else {
+        outputTextArea.innerHTML = "";
+        port = new SerialPort(path, {
+            bitrate: parseInt(baudrate), ctsFlowControl: true, persistent: true,
+        });
+        port.addEventListener(SerialPort.EVENT.STATE_CHANGE, {notify: notify});
+        port.addEventListener(SerialPort.EVENT.DATA_AVAILABLE, monitor);
+        port.connect();
+    }
 }
 
 function notify(newState) {
-  statusText.innerHTML = port.stateMessage();
-  if (newState == SerialPort.STATE.CONNECTED) {
-    connectButton.innerHTML = "Disconnect";
-  } else {
-    connectButton.innerHTML = "Connect";
-  }
+    statusText.innerHTML = port.stateMessage();
+    if (newState === SerialPort.STATE.CONNECTED) {
+        connectButton.innerHTML = "Disconnect";
+    } else {
+        connectButton.innerHTML = "Connect";
+    }
+}
+
+function getEndingLine() {
+    const option = lineEndingSelect.options[lineEndingSelect.selectedIndex].value;
+    return {
+        "NONE": "", "NL": "\n", "CR": "\r", "BOTH": "\n\r"
+    }[option];
 }
 
 function send() {
-  if (port && port.isConnected()) {
-    port.writeString(inputText.value + lineEndingSelect.options[lineEndingSelect.selectedIndex].value);
-    inputText.value = "";
-  }
+    if (port && port.isConnected()) {
+        port.writeString(inputText.value + getEndingLine());
+        inputText.value = "";
+    }
 }
 
 function attachEvents() {
-  connectButton.addEventListener("click", function () {
-    try {
-      var port = portsSelect.options[portsSelect.selectedIndex].value;
-      var baudrate = baudrateSelect.options[baudrateSelect.selectedIndex].value;
-      connect(port, baudrate);
-    } catch (e) {
-      statusText.innerHTML = "No port to connect. " + e;
-    }
-  });
-  sendButton.addEventListener("click", function () {
-    send();
-  });
-  inputText.addEventListener("keypress", function (e) {
-    if (e.which == 13) {
-      send();
-    }
-  });
-  autoScrollInput.addEventListener("click", function () {
-    if (monitor) {
-      monitor.invertAutoScroll();
-    }
-  });
+    connectButton.addEventListener("click", function () {
+        try {
+            var port = portsSelect.options[portsSelect.selectedIndex].value;
+            var baudrate = baudrateSelect.options[baudrateSelect.selectedIndex].value;
+            connect(port, baudrate);
+        } catch (e) {
+            statusText.innerHTML = "No port to connect. " + e;
+        }
+    });
+    sendButton.addEventListener("click", function () {
+        send();
+    });
+    inputText.addEventListener("keypress", function (e) {
+        if (e.which === 13) {
+            send();
+        }
+    });
+    autoScrollInput.addEventListener("click", function () {
+        if (monitor) {
+            monitor.invertAutoScroll();
+        }
+    });
 }
 
 function extractPaths(ports) {
-  var paths = [];
-  ports.map(function (port) {
-    paths.push(port.path);
-  });
-  return paths;
+    var paths = [];
+    ports.map(function (port) {
+        paths.push(port.path);
+    });
+    return paths;
 }
 
 window.addEventListener("load", function () {
-  if (typeof chrome != "undefined" && chrome.serial) {
-    initComponents();
-    attachEvents();
-    setInterval(function () {
-      chrome.serial.getDevices(function (ports) {
-        var paths = extractPaths(ports);
-        if (port && port.isConnected() && paths.indexOf(port.getPath()) < 0) {
-          port.disconnect();
-        }
-        if (!areArraysEqual(previousPaths, paths)) {
-          listPorts(ports);
-          previousPaths = paths;
-        }
-      });
-    }, 1000);
-  }
+    if (typeof chrome != "undefined" && chrome.serial) {
+        initComponents();
+        attachEvents();
+        setInterval(function () {
+            chrome.serial.getDevices(function (ports) {
+                var paths = extractPaths(ports);
+                if (port && port.isConnected() && paths.indexOf(port.getPath()) < 0) {
+                    port.disconnect();
+                }
+                if (!areArraysEqual(previousPaths, paths)) {
+                    listPorts(ports);
+                    previousPaths = paths;
+                }
+            });
+        }, 1000);
+    }
 }, false);
--- notifier.js
+++ notifier.js
@@ -1,20 +1,25 @@
 var EventNotifier = function () {
-  this.listeners = {};
+    this.listeners = {};
+};
+
+EventNotifier.prototype.clear = function () {
+    this.listeners = {};
 };
 
 EventNotifier.prototype.addEventListener = function (event, listener) {
-  if (!this.listeners[event]) {
-    this.listeners[event] = [];
-  }
-  this.listeners[event].push(listener);
+    if (!this.listeners[event]) {
+        this.listeners[event] = [];
+    }
+    this.listeners[event].push(listener);
 };
 
 EventNotifier.prototype.notifyEvent = function (event, notification) {
-  var listeners = this.listeners[event];
-  if (listeners != null) {
-    listeners.map(function (listener) {
-      listener.notify(notification);
-    });
-  }
+    var listeners = this.listeners[event];
+    console.log("this.listeners: ", this.listeners)
+    if (listeners != null) {
+        listeners.map(function (listener) {
+            listener.notify(notification);
+        });
+    }
 };
 
